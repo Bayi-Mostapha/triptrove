6283611fa0fb3698577323caf6d6d50a1f2563df
elkhayatireda <elkhayatireda2@gmail.com>
2024-05-08 07:26:06 +0100

    add admin model

---
+++ server/models/admin.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const adminSchema = mongoose.Schema(
+    {
+        firstName: {
+            type: String ,
+            required: true,
+        },
+        lastName: {
+            type: String ,
+            required: true,
+        },
+        email: {
+            type: String ,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String ,
+            required: true,
+        },
+        role: { type: String, enum: ['admin', 'superAdmin'], default: 'admin' },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+ const Admin = mongoose.model('Admin', adminSchema);
+ 
+export default Admin;
