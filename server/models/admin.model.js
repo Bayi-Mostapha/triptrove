cb51a35f73654a6459b3c43ade2746d05629ee7a
elkhayatireda <elkhayatireda2@gmail.com>
2024-05-11 18:21:39 +0100

    add full name to admin model

--- server/models/admin.model.js
+++ server/models/admin.model.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const adminSchema = mongoose.Schema(
     {
+        fullName: {
+            type: String ,
+            required: true,
+        },
         firstName: {
             type: String ,
             required: true,
